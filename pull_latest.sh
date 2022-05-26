5c8c2b0c9b03d0f36a075b866e6b4c4a3a1a01cb
Dan <dan@Dans-MacBook-Pro.local>
2022-05-26 11:33:55 -0500

    update script to only attempt git pull if it is a directory
      Committer: Dan Metzler<dan@helixsoft.com>

--- pull_latest.sh
+++ pull_latest.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 function update_all() {
-  for dir in *; do
-    echo
-    printf $dir
-    echo
-    cd "$dir" && git pull
-    cd ..
+  for dir in *; do 
+    if [ -d $dir ]
+    then
+      echo
+      printf $dir
+      echo
+      cd "$dir" && git pull
+      cd ..
+    fi
   done
 }
 
